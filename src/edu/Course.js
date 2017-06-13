0581a339a15c78c142437b0083efef95c6b9f472
Aman Gupta <aman.a.gupta1989@gmail.com>
2017-06-13 12:25:33 +0530

    added additional paragraph

--- src/edu/Course.js
+++ src/edu/Course.js
@@ -7,7 +7,8 @@ export default class Course extends Component {
         return (
             <div key={course.id} className={isDisabled ? "block" : "block drags"}>
                 <h4>{course.name}</h4>
-                <span>4 credits</span>
+                <span>14 credits</span>
+                <p>Some random text here</p>
             </div>
         );
     }
