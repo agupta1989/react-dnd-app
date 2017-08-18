903b40b0600a3701a9066a0a076b1786a2d4242b
Aman Gupta <aman.a.gupta1989@gmail.com>
2017-08-18 18:43:54 +0530

    changes random

--- src/edu/Term.js
+++ src/edu/Term.js
@@ -1,10 +1,26 @@
 import React, {Component} from "react";
+import { ItemTypes } from './Constant';
 import DraggableCourse from "./dnd/DraggableCourse";
-import { DragDropContext } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
+import { DropTarget } from 'react-dnd';
 import BlankCourse from "./dnd/BlankCourse";
 
-// Some random comments for agpt/some_random_hotfix
+const dropTarget = {
+    hover(props, monitor, component) {
+        console.log("~~~~~~~ onHover ~~~~~~~");
+        console.log("props", props);
+        console.log("monitor", monitor);
+        console.log("component", component);
+        console.log("~~~~~~~ #### ~~~~~~~");
+    },
+    drop(props, monitor, component) {
+        console.log("~~~~~~~ onDrop ~~~~~~~");
+        console.log("props", props);
+        console.log("monitor", monitor);
+        console.log("component", component);
+        console.log("~~~~~~~ #### ~~~~~~~");
+    }
+};
+
 class Term extends Component {
     constructor(props) {
         super(props);
@@ -18,23 +34,29 @@ class Term extends Component {
         this.props.onDrop(dropInfo);
     }
     render() {
-        const { term } = this.props;
+        const { term, connectDropTarget } = this.props;
 
         return (
-            <div className="col">
-                <div className="block">
-                    <h4> {term.name} </h4>
+            connectDropTarget(
+                <div className="col term">
+                    <div className="block">
+                        <h4> {term.name} </h4>
+                    </div>
+                    {
+                        term.courses.length < 1
+                            ? <BlankCourse index={0} termId={term.id} moveCard={this.moveCard} updateCard={this.updateCard} />
+                            : term.courses.map((course, idx) => (
+                                <DraggableCourse isDisabled={course.isDisabled} termId={term.id} key={course.id} index={idx} course={course} moveCard={this.moveCard} updateCard={this.updateCard} />
+                            ))
+                    }
                 </div>
-                {
-                    term.courses.length < 1
-                        ? <BlankCourse index={0} termId={term.id} moveCard={this.moveCard} updateCard={this.updateCard} />
-                        : term.courses.map((course, idx) => (
-                            <DraggableCourse isDisabled={course.isDisabled} termId={term.id} key={course.id} index={idx} course={course} moveCard={this.moveCard} updateCard={this.updateCard} />
-                        ))
-                }
-            </div>
+            )
         );
     }
 }
 
-export default DragDropContext(HTML5Backend)(Term);
+export default DropTarget(ItemTypes.COURSE, dropTarget, (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+}))(Term);
--- src/edu/dnd/DraggableCourse.js
+++ src/edu/dnd/DraggableCourse.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 import { ItemTypes } from '../Constant';
-import { DragSource, DropTarget } from 'react-dnd';
-import { findDOMNode } from "react-dom";
+import { DragSource } from 'react-dnd';
 import Course from '../Course';
 
 const getStyles = (props) => {
-    const { left, top, isDragging, isDisabled } = props;
+    const { isDragging, isDisabled } = props;
     const opacity = (isDisabled || isDragging) ? 0.5 : 1;
 
     return {
@@ -30,73 +29,23 @@ const dragSource = {
     }
 };
 
-const dropTarget = {
-  hover(props, monitor, component) {
-    const courseItem = monitor.getItem();
-    const dragIndex = courseItem.index;
-    const hoverIndex = props.index;
-    const termId = props.termId;
-
-    // Don't replace items with themselves
-    if (termId === courseItem.course.termId && dragIndex === hoverIndex) {
-      return;
-    }
-
-    // Determine rectangle on screen
-    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
-
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-    // Determine mouse position
-    const clientOffset = monitor.getClientOffset();
-
-    // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-    // Dragging downwards
-    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-      return;
-    }
-
-    // Dragging upwards
-    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-      return;
-    }
-
-    props.moveCard({dragIndex, hoverIndex, termId, course: courseItem.course});
-
-    // Note: we're mutating the monitor item here!
-    // Generally it's better to avoid mutations,
-    // but it's good here for the sake of performance
-    // to avoid expensive index searches.
-    monitor.getItem().index = hoverIndex;
-  },
-  drop(props, monitor, component) {
-      console.log("props", props);
-  }
-};
-
-
 class DraggableCourse extends Component {
     render() {
-        const { connectDragSource, connectDropTarget, course, index, isDisabled } = this.props;
+        const { connectDragSource, course, index, isDisabled } = this.props;
         return (
-            connectDragSource(connectDropTarget(
+            connectDragSource(
                 <div style={getStyles(this.props)}>
                     <Course isDisabled={isDisabled} course={course} index={index}/>
                 </div>
-            ))
+            )
         );
     }
 }
 
-export default DropTarget(ItemTypes.COURSE, dropTarget, connect => ({
-  connectDropTarget: connect.dropTarget(),
-}))(DragSource(ItemTypes.COURSE, dragSource, (connect, monitor) => {
+export default DragSource(ItemTypes.COURSE, dragSource, (connect, monitor) => {
   return {
     connectDragSource: connect.dragSource(),
     isDragging: monitor.isDragging()
   }
-})(DraggableCourse));
+})(DraggableCourse);
 
